e71bdb06106574984cd1b6f68abad3a08744624f
JuliaPivovarova <83279528+JuliaPivovarova@users.noreply.github.com>
2021-09-30 01:37:14 +0300

    Hw2
    asteroids game

---
+++ Assets/Code/Fire.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Code
+{
+    internal sealed class Fire
+    {
+        private readonly GameObject _bullet;
+        private readonly Transform _barrel;
+
+        public Fire(){}
+        
+        public Fire(GameObject bullet, Transform barrel)
+        {
+            _bullet = bullet;
+            _barrel = barrel;
+        }
+
+        public void SingleFire(float force)
+        {
+            var temAmmunition = InstantiateBullet.InstBullet(_bullet, _barrel);
+            temAmmunition.SetActive(true);
+            temAmmunition.GetComponent<Rigidbody2D>().AddForce(_barrel.up * force);
+        }
+    }
+}
---
+++ Assets/Code/UI/DisplayHp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Code.UI
+{
+    internal sealed class DisplayHp
+    {
+        private string _hptext;
+
+        public DisplayHp()
+        {
+            _hptext = String.Empty;
+        }
+
+        public string Display(float hpMax, float hpValue)
+        {
+            _hptext = $"У вас {hpValue} из {hpMax}";
+            return _hptext;
+        }
+    }
+}
